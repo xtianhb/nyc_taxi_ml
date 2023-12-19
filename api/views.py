b3ee0c93a45f9494e31e64255e413ca50ddf6e07
josedavidnup <josedavidnup@gmail.com>
2023-12-19 17:16:05 -0400

    predict route

--- api/views.py
+++ api/views.py
@@ -15,3 +15,24 @@ router = Blueprint("app_router", __name__, template_folder="templates")
 @router.route("/")
 def hello_world():
     return render_template("index.html")
+
+
+@router.route("/predict", methods=["POST"])
+def predict():
+    if request.method == "POST":
+        # Get input data from the form
+        trip_distance = float(request.form["trip_distance"])
+        pickup_date = request.form["pickup_date"]
+        time = request.form["time"]
+
+        # # Use the ML model to make predictions
+        # prediction = predict_taxi_fare_duration(
+        #     trip_distance, pickup_date, weather, time, transit
+        # )
+
+        # The following variables are fixed because we do not have a model function yet.
+        fare = 20.0
+        duration = 15
+
+        # Pass the prediction to the result page
+        return render_template("result.html", fare=fare, duration=duration)
